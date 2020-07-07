aea157f3516d9cc0e1e712e32a8b56cec8b94227
Graham Daws <graham.daws@gmail.com>
2020-07-07 15:36:48 +0100

    Add test cases for the frame/protocol module

---
+++ src/frame/protocol.test.ts
@@ -0,0 +1,47 @@
+import { acceptedVersions, supportedProtocolVersion, acceptedAckModes, getAckMode } from './protocol';
+
+test('acceptedVersions', () => {
+
+  const versions = acceptedVersions();
+
+  expect(versions[0]).toBe('1.0');
+  expect(versions[1]).toBe('1.1');
+  expect(versions[2]).toBe('1.2');
+});
+
+test('supportedProtocolVersion', () => {
+
+  expect(supportedProtocolVersion('1.0')).toBe('1.0');
+  expect(supportedProtocolVersion('1.1')).toBe('1.1');
+  expect(supportedProtocolVersion('1.2')).toBe('1.2');
+
+  expect(supportedProtocolVersion('blah')).toBeUndefined();
+});
+
+test('acceptedAckModes', () => {
+
+  const ackModes = acceptedAckModes('1.0');
+
+  expect(acceptedAckModes('1.0')).toEqual(['auto', 'client', 'client-individual']);
+  expect(acceptedAckModes('1.1')).toEqual(['auto', 'client', 'client-individual']);
+  expect(acceptedAckModes('1.2')).toEqual(['auto', 'client', 'client-individual']);
+
+});
+
+test('getAckMode', () => {
+
+  expect(getAckMode(undefined, '1.0')).toBe('auto');
+  expect(getAckMode('client', '1.0')).toBe('client');
+  expect(getAckMode('client-individual', '1.0')).toBe('client-individual');
+  expect(getAckMode('blah', '1.0')).toBeUndefined();
+
+  expect(getAckMode(undefined, '1.1')).toBe('auto');
+  expect(getAckMode('client', '1.1')).toBe('client');
+  expect(getAckMode('client-individual', '1.1')).toBe('client-individual');
+  expect(getAckMode('blah', '1.1')).toBeUndefined();
+
+  expect(getAckMode(undefined, '1.2')).toBe('auto');
+  expect(getAckMode('client', '1.2')).toBe('client');
+  expect(getAckMode('client-individual', '1.2')).toBe('client-individual');
+  expect(getAckMode('blah', '1.2')).toBeUndefined();
+});
