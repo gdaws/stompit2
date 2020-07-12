192c87a3d361cf0c0bba9a322cbbef0327621c3d
Graham Daws <graham.daws@gmail.com>
2020-07-13 00:01:41 +0100

    Wait for RabbitMQ to start STOMP listener

---
+++ test/broker/rabbitmq/wait_until_listening.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+SECONDS=0
+
+while [ $SECOND -lt 60 ]
+do
+  if docker logs stompit2_rabbitmq_server | grep -q "started STOMP TCP listener"; then
+    exit 0
+  fi
+  sleep 1
+done
+
+exit 1
